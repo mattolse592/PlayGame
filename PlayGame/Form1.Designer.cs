eb4d9f9235dbd09f6be28aa2ed8c80503bbd8326
Matthew Olsen <mattolse592@ed.amdsb.ca>
2022-09-15 12:44:38 -0400

    Add project files.

---
+++ PlayGame/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace PlayGame
+{
+    partial class normalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(normalForm));
+            this.playButton = new System.Windows.Forms.Button();
+            this.startingLabel = new System.Windows.Forms.Label();
+            this.greenColor = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.scoreButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Black;
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("Miriam Mono CLM", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(100, 105);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(279, 145);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "PLAY GAME";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // startingLabel
+            // 
+            this.startingLabel.AutoSize = true;
+            this.startingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startingLabel.Location = new System.Drawing.Point(110, 299);
+            this.startingLabel.Name = "startingLabel";
+            this.startingLabel.Size = new System.Drawing.Size(269, 31);
+            this.startingLabel.TabIndex = 1;
+            this.startingLabel.Text = "Game is starting in: 3";
+            this.startingLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.startingLabel.Visible = false;
+            // 
+            // greenColor
+            // 
+            this.greenColor.BackColor = System.Drawing.Color.DarkGreen;
+            this.greenColor.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenColor.Location = new System.Drawing.Point(-54, -52);
+            this.greenColor.Name = "greenColor";
+            this.greenColor.Size = new System.Drawing.Size(609, 545);
+            this.greenColor.TabIndex = 2;
+            this.greenColor.Text = "GO!!!";
+            this.greenColor.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.greenColor.Visible = false;
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Red;
+            this.exitButton.Cursor = System.Windows.Forms.Cursors.No;
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Abort;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exitButton.Font = new System.Drawing.Font("Noto Naskh Arabic", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(349, 12);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(113, 46);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "EXIT";
+            this.exitButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // scoreButton
+            // 
+            this.scoreButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.scoreButton.Location = new System.Drawing.Point(172, 172);
+            this.scoreButton.Name = "scoreButton";
+            this.scoreButton.Size = new System.Drawing.Size(131, 112);
+            this.scoreButton.TabIndex = 4;
+            this.scoreButton.Text = "Score: ";
+            this.scoreButton.UseVisualStyleBackColor = true;
+            this.scoreButton.Click += new System.EventHandler(this.scoreButton_Click);
+            // 
+            // normalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(474, 429);
+            this.Controls.Add(this.scoreButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.greenColor);
+            this.Controls.Add(this.startingLabel);
+            this.Controls.Add(this.playButton);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "normalForm";
+            this.Opacity = 0.99D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Play Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label startingLabel;
+        private System.Windows.Forms.Label greenColor;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button scoreButton;
+    }
+}
+
---
+++ PlayGame/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PlayGame
+{
+    public partial class normalForm : System.Windows.Forms.Form
+    {
+        public normalForm()
+        {
+            InitializeComponent();
+            scoreButton.Hide(); 
+            
+        }
+
+        
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            playButton.Hide();
+            greenColor.Hide(); 
+
+            SoundPlayer goPlayer = new SoundPlayer(Properties.Resources.readyGo);
+            goPlayer.Play();
+
+            Thread.Sleep(900);
+            
+            startingLabel.Show();
+
+            Refresh();
+
+            Thread.Sleep(1000);
+            startingLabel.Text = "Game is starting in: 2";
+            Refresh();
+
+            Thread.Sleep(1000);
+            startingLabel.Text = "Game is starting in: 1";
+            Refresh();
+
+            Thread.Sleep(1000);
+
+            startingLabel.Text = "";
+            greenColor.Show();
+            scoreButton.Show();
+            exitButton.Show();
+            Refresh();
+
+           
+            
+            Refresh();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            SoundPlayer rickPlayer = new SoundPlayer(Properties.Resources.rick);
+
+            rickPlayer.Play();
+
+            Thread.Sleep(2000);
+            System.Windows.Forms.Application.ExitThread();
+        }
+        int score = 0;
+        private void scoreButton_Click(object sender, EventArgs e)
+        { 
+            score = score + 1;
+
+            scoreButton.Text = "Score: " + score;
+        }
+    }
+}
